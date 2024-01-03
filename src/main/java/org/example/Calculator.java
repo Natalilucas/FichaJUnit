13c5bc0b1fe5e8f0ab51c03c30c822c0b49cec34
Natali Lucas <1222649@isep.ipp.pt>
2024-01-03 17:01:36 +0000

    [Junit Tests]

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Calculator {
+
+    public int add( int a, int b){
+        return a + b;
+    }
+
+    public int divide( int a, int b){
+        if(b == 0 ){
+            throw new IllegalArgumentException("Valor negativo");
+        }
+        return a/b;
+    }
+
+    public int multiply( int a, int b){
+        return a*b;
+    }
+
+    public int minus( int a, int b){
+        return a-b;
+    }
+
+
+
+}
